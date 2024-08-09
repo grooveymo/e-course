97f5eebd8f88ce07b51282fae79bd8dd7614a3f0
mo sayed <grooveymo@gmail.com>
2024-08-09 23:06:21 +0100

    fixed - links to courses page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,18 @@ const Header: React.FC = () => {
     <header>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/about">Courses</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
     </header>
