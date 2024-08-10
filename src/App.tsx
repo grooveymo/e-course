6c2357144264a53734a7a54942b73704f03a0f89
mo sayed <grooveymo@gmail.com>
2024-08-10 21:50:38 +0100

    feat: add reactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,25 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Courses from './pages/Courses';
+// Import the QueryClient and QueryClientProvider from react-query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const App: React.FC = () => {
+  // Create a client
+  const queryClient = new QueryClient();
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="courses" element={<Courses />} />
-        </Route>
+        {/** Provide the client to your App  */}
+        <QueryClientProvider client={queryClient}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="courses" element={<Courses />} />
+          </Route>
+        </QueryClientProvider>
       </Routes>
     </Router>
   );
