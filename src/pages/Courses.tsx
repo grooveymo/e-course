b3a0c4f3f41899d88086239401269f547eee295f
mo sayed <grooveymo@gmail.com>
2024-08-10 21:18:40 +0100

    feat: fetched data with useEffect

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const fetchCourses = async () => {
+  const response = await fetch('http://localhost:3000/courses');
+  const data = await response.json();
+  return data;
+};
 
 const Courses: React.FC = () => {
+  let data;
+  useEffect(() => {
+    data = fetchCourses().then((data) => console.log(data));
+  }, []);
+
+  console.log('data:', data);
   return <h1>Welcome to the Courses Page</h1>;
 };
 
