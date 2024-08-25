46fa6135c528d296eb8e892aaca5ef9151b62921
mo sayed <grooveymo@gmail.com>
2024-08-25 19:58:08 +0100

    feat: wired in call to fetch course details on the EditCourse page

--- src/pages/EditCourse.tsx
+++ src/pages/EditCourse.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { fetchCourse } from '../services/courses';
+
+type RouteParams = {
+  id: string;
+};
 
 const EditCourse = () => {
+  const { id } = useParams<RouteParams>();
+
+  const { data } = useQuery({
+    queryKey: ['course', id],
+    queryFn: () => fetchCourse(id as string),
+    enabled: !!id, // Only run the query if we have an id
+  });
+
+  console.log('>>> data:', data);
   return (
     <div>
       <h1>Edit Course</h1>
--- src/services/courses.ts
+++ src/services/courses.ts
@@ -1,4 +1,5 @@
 import { Course } from '../types/course';
+import { QueryFunctionContext } from '@tanstack/react-query';
 
 export const fetchCourses = async () => {
   const response = await fetch('http://localhost:3000/courses', {
@@ -29,4 +30,23 @@ export const createCourse = async (courseData: Course): Promise<Course> => {
   }
 
   return await response.json();
+};
+
+export const fetchCourse = async (id: string): Promise<Course> => {
+
+  if (!id) {
+    throw new Error('Course ID is required');
+  }
+  const response = await fetch(`http://localhost:3000/courses/${id}`, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  if (response.status !== 200) {
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json();
+  return data;
 };
