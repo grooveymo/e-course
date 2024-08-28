90f2c7e4fc9b34ddb0c2c93e4477cf725a6ab307
mo sayed <grooveymo@gmail.com>
2024-08-28 10:56:09 +0100

    feat: added checkbox component

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export interface CheckboxProps {
+  label: string;
+  isChecked: boolean;
+  setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const Checkbox = ({
+  label,
+  isChecked = false,
+  setIsChecked,
+}: CheckboxProps) => {
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div className="checkbox-container">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+        className="checkbox-input"
+      />
+      <label className="checkbox-label">{label}</label>
+    </div>
+  );
+};
+
+export default Checkbox;
