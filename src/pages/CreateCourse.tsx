5c3bd896e561c6c1b8e5196a88bd8a58c5c0f80a
mo sayed <grooveymo@gmail.com>
2024-08-28 19:04:51 +0100

    chore: rename initialState

--- src/pages/CreateCourse.tsx
+++ src/pages/CreateCourse.tsx
@@ -2,7 +2,7 @@ import { Input } from '../components/Input';
 import Form from '../components/Form';
 import Button from '../components/Button';
 import { useReducer } from 'react';
-import { courseReducer, initialCreateState } from '../hooks/courseReducer';
+import { courseReducer, initialFormState } from '../hooks/courseReducer';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createCourse } from '../services/courses';
 import { useNavigate } from 'react-router-dom';
@@ -11,7 +11,7 @@ const CreateCourse = () => {
   //----------------------------------------------------
   // Employ the useReducer hook to manage the form state
   //----------------------------------------------------
-  const [state, dispatch] = useReducer(courseReducer, initialCreateState);
+  const [state, dispatch] = useReducer(courseReducer, initialFormState);
 
   //------------------------------------------------------
   // Employ React Query's mutation hook
